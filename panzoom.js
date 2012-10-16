14568645b99b489e31432eabfe45faee29cf02eb
Rob Ashton <robashton@codeofrob.com>
2012-10-16 17:32:13 +0200

    Re-commit in order to have sensible API over time control

--- panzoom.js
+++ panzoom.js
@@ -21,7 +21,9 @@
        })
      )
 
-     var updateCss = function() {
+     var updateCss = function(time) {
+       time = typeof time === 'undefined' ? options.transition : time
+       $img.css({'-webkit-transition': '-webkit-transform ' + time + 's'})
 
        if(focalx < 0) focalx = 0
        if(focaly < 0) focaly = 0
@@ -37,31 +39,30 @@
          '-webkit-transform-origin': '0% 0%',
          '-webkit-transform': 'translate(' + adjustedFocalX + 'px,' + adjustedFocalY + 'px) ' + 
                               'scale(' + scale + ',' + scale + ')',
-         '-webkit-transition': '-webkit-transform ' + options.transition + 's' 
        })
      }
-     function reset() {
+     function reset(time) {
        scale = 1.0
        focalx = rawImageWidth / 2.0 
        focaly = rawImageHeight / 2.0 
-       updateCss()
+       updateCss(time)
      }
      reset()
 
      return {
-       pan: function(x, y) {
-         focalx += x 
-         focaly += y 
-         updateCss()
+       pan: function(x, y, time) {
+         focalx += x  / scale
+         focaly += y  / scale
+         updateCss(time)
        },
-       panTo: function(x, y) {
+       panTo: function(x, y, time) {
          focalx = x
          focaly = y
-         updateCss()
+         updateCss(time)
        },
-       zoomTo: function(level) {
+       zoomTo: function(level, time) {
          scale = level
-         updateCss()
+         updateCss(time)
        },
        reset: reset,
        screenToImage: function(x, y) {
